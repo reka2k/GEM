d3afab36813984e506149fc0c32f367c909bb589
KelyanRs <121857444+KelyanRs@users.noreply.github.com>
2023-01-03 16:19:40 +0100

    Ajout ENtity user/compteur

--- gemCommon/src/main/java/lml/snir/gem/common/metier/entity/User.java
+++ gemCommon/src/main/java/lml/snir/gem/common/metier/entity/User.java
@@ -3,12 +3,94 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lml.snir.gem.common.metier.entity;
+package lml.snir.gem.metier.entity;
 
 /**
  *
- * @author david
+ * @author lml
  */
-class User {
-    
+public class User {
+
+    private String nom;
+    private String prenom;
+    private String login;
+    private String mdp;
+    private int id;
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * @return the prenom
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * @param prenom the prenom to set
+     */
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the mdp
+     */
+    public String getMdp() {
+        return mdp;
+    }
+
+    /**
+     * @param mdp the mdp to set
+     */
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String encryptedMdp(String mdp) {
+        //A completer
+        this.mdp = mdp + 1;
+        return mdp;
+    }
+
 }
