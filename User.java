5e733e45f28c9764d0ce2e5cdf85b2e208107967
KelyanRs <121857444+KelyanRs@users.noreply.github.com>
2023-01-03 17:07:38 +0100

    Ajout méthode de cryptage in User

---
+++ User.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lml.snir.gem.common.metier.entity;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author lml
+ */
+public class User {
+
+    private String nom;
+    private String prenom;
+    private String login;
+    private String mdp;
+    private int id;
+
+    /**
+     * @return the nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * @param nom the nom to set
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * @return the prenom
+     */
+    public String getPrenom() {
+        return prenom;
+    }
+
+    /**
+     * @param prenom the prenom to set
+     */
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    /**
+     * @return the login
+     */
+    public String getLogin() {
+        return login;
+    }
+
+    /**
+     * @param login the login to set
+     */
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    /**
+     * @return the mdp
+     */
+    public String getMdp() {
+        return mdp;
+    }
+
+    /**
+     * @param mdp the mdp to set
+     */
+    public void setMdp(String mdp) {
+        this.mdp = mdp;
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String encryptedMdp(String mdp) {
+        //A completer
+        this.hashPassword(this.mdp);
+        return mdp;
+    }
+    
+//Renvoi le mdp crypter
+  public static String hashPassword(String password) {
+    String hashedPassword = null;
+    try {
+      MessageDigest md = MessageDigest.getInstance("SHA-256");
+      md.update(password.getBytes());
+      byte[] bytes = md.digest();
+      StringBuilder sb = new StringBuilder();
+      for(int i=0; i< bytes.length ;i++) {
+        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+      }
+      hashedPassword = sb.toString();
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+    return hashedPassword;
+  
+}
+
+}
