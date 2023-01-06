0453d0169409d870fa96ad8e96682498b5bad32f
KelyanRs <121857444+KelyanRs@users.noreply.github.com>
2023-01-06 10:26:15 +0100

    Add files via upload

---
+++ gemCommon/src/main/java/lml/snir/gem/common/metier/transactionel/CapteurHumiditeService.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lml.snir.gem.common.metier.transactionel;
+
+import lml.snir.gem.common.metier.entity.CapteurHumidite;
+import lml.snir.persistence.CrudService;
+
+/**
+ *
+ * @author lml
+ */
+public interface CapteurHumiditeService extends CrudService<CapteurHumidite>{
+    //Ajout méthode a voir ensemble
+}
