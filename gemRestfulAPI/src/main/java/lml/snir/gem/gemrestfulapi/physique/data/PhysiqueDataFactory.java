cf16df16a86dff4ee90b42cc651e65d7f6d932f1
David Macario <reka2k6@gmail.com>
2023-01-10 14:29:40 +0100

    Persistence + début PhysiqueFactory

---
+++ gemRestfulAPI/src/main/java/lml/snir/gem/gemrestfulapi/physique/data/PhysiqueDataFactory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lml.snir.gem.gemrestfulapi.physique.data;
+
+/**
+ *
+ * @author david
+ */
+public class PhysiqueDataFactory {
+    private static final String PU = "lml.snir.GEM_GEM_war_1.0PU";
+    
+    private PhysiqueDataFactory(){
+        
+    }
+    
+    private static CapteurHumiditeDataService capteurHumiditeDataService = null;
+    
+    public synchronized static CapteurHumiditeDataService getCapteurHumiditeDataService(){
+        if(capteurHumiditeDataService == null) capteurHumiditeDataService = new CapteurHumiditeDataServiceJPAImpl(PU);
+        return capteurHumiditeDataService;
+    }
+    
+    private static CapteurTemperatureDataService capteurTemperatureDataService = null;
+    
+    public synchronized static CapteurTemperatureDataService getCapteurTemperatureDataService(){
+        if(capteurTemperatureDataService == null) capteurTemperatureDataService = new CapteurTemperatureDataServiceJPAImpl(PU);
+        return capteurTemperatureDataService;
+    }
+}
