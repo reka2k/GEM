973ed0fadce2683c56f6cb4d5069a34f019914b5
David Macario <reka2k6@gmail.com>
2023-01-27 11:54:09 +0100

    REST API fini

--- gemRestfulAPI/src/main/java/lml/snir/gem/gemrestfulapi/api/VmcFacadeREST.java
+++ gemRestfulAPI/src/main/java/lml/snir/gem/gemrestfulapi/api/VmcFacadeREST.java
@@ -1,9 +1,14 @@
 package lml.snir.gem.gemrestfulapi.api;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonBuilderFactory;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -13,7 +18,13 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import lml.snir.gem.common.metier.entity.Vmc;
+import lml.snir.gem.common.metier.transactionel.VmcService;
+import lml.snir.gem.gemrestfulapi.transactionel.VmcServiceImpl;
 
 /**
  *
@@ -23,65 +34,116 @@ import lml.snir.gem.common.metier.entity.Vmc;
 @Path("/vmc")
 public class VmcFacadeREST extends AbstractFacade<Vmc> {
 
-    //@PersistenceContext(unitName = "lml.snir.GEM_GEM_war_1.0PU")
     private EntityManager em;
+    private VmcService vmcService;
 
     public VmcFacadeREST() {
         super(Vmc.class);
+        this.vmcService = new VmcServiceImpl();
     }
 
     @POST
     @Override
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
     public void create(Vmc entity) {
         super.create(entity);
     }
 
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Path("/insert")
+    public Response insert(Vmc vmc) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        final Map<String, ?> config = Collections.emptyMap();
+        JsonBuilderFactory factory = Json.createBuilderFactory(config);
+        try {
+            this.vmcService.add(vmc);
+            JsonObject jsonString = factory.createObjectBuilder().add("message", "Data inserted successfully")
+                    .build();
+            return Response.ok(gson.toJson(jsonString), MediaType.APPLICATION_JSON).type("application/json").build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     @PUT
-    @Path("{id}")
-    @Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public void edit(@PathParam("id") Long id, Vmc entity) {
-        super.edit(entity);
+    @Path("edit/{id}")
+    @Consumes({ MediaType.APPLICATION_JSON })
+    public Response edit(@PathParam("id") Long id, Vmc vmc) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        final Map<String, ?> config = Collections.emptyMap();
+        JsonBuilderFactory factory = Json.createBuilderFactory(config);
+        try {
+            this.vmcService.update(vmc);
+            JsonObject jsonString = factory.createObjectBuilder().add("message", "Data edited successfully")
+                    .build();
+            return Response.ok(gson.toJson(jsonString), MediaType.APPLICATION_JSON).type("application/json").build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @DELETE
-    @Path("{id}")
-    public void remove(@PathParam("id") Long id) {
-        super.remove(super.find(id));
+    @Path("remove/{id}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response remove(@PathParam("id") Long id) {
+        Gson gson = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        final Map<String, ?> config = Collections.emptyMap();
+        JsonBuilderFactory factory = Json.createBuilderFactory(config);
+        try {
+            this.vmcService.remove(this.vmcService.getById(id));
+            JsonObject jsonString = factory.createObjectBuilder().add("message", "Data removed successfully").build();
+            return Response.ok(gson.toJson(jsonString), MediaType.APPLICATION_JSON).type("application/json").build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
     @GET
-    @Path("{id}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public Vmc find(@PathParam("id") Long id) {
-        return super.find(id);
+    @Path("getById/{id}")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response findVmc(@PathParam("id") Long id) {
+        try {
+            return Response.ok(new Gson().toJson(this.vmcService.getById(id))).build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+
     }
 
     @GET
     @Override
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    @Path("getAll")
+    @Produces({ MediaType.APPLICATION_JSON })
     public List<Vmc> findAll() {
         return super.findAll();
     }
 
     @GET
-    @Path("{from}/{to}")
-    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
-    public List<Vmc> findRange(@PathParam("from") Integer from, @PathParam("to") Integer to) {
-        return super.findRange(new int[]{from, to});
-    }
-
-    @GET
-    @Path("count")
-    @Produces(MediaType.TEXT_PLAIN)
-    public String countREST() {
-        return String.valueOf(super.count());
+    @Produces({ MediaType.APPLICATION_JSON })
+    @Path("/getAll")
+    public Response getAll() {
+        try {
+            return Response.ok(new Gson().toJson(this.vmcService.getAll())).build();
+        } catch (Exception e) {
+            System.out.println(e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
     @Override
     protected EntityManager getEntityManager() {
         return em;
     }
-    
+
 }
